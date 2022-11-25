3bacb4b88a0d2cec1fc25880f30cffcb48a5aec6
LongHD <longhd240195@gmail.com>
2022-11-25 10:25:58 +0700

    - import apple auth

--- Assets/Network/GooglePlayGameControl.cs
+++ Assets/Network/GooglePlayGameControl.cs
@@ -1,5 +1,18 @@
-﻿using GooglePlayGames;
+﻿#if UNITY_ANDROID
+using GooglePlayGames;
 using GooglePlayGames.BasicApi;
+#endif
+
+#if UNITY_IOS
+using AppleAuth;
+using AppleAuth.Enums;
+using AppleAuth.Interfaces;
+using AppleAuth.Native;
+using System.Text;
+
+using UnityEditor.PackageManager;
+#endif
+
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
@@ -7,13 +20,32 @@ public class GooglePlayGameControl : MonoBehaviour
 {
     private string Token;
 
+#if UNITY_IOS
+    IAppleAuthManager m_AppleAuthManager;
+#endif
+
+
     void Awake()
     {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+#if UNITY_ANDROID
         PlayGamesPlatform.Activate();
+#endif
+
+#if UNITY_IOS
+        var deserializer = new PayloadDeserializer();
+        m_AppleAuthManager = new AppleAuthManager(deserializer);
+#endif
     }
 
     public void LoginGooglePlayGames()
     {
+#if UNITY_ANDROID
+
         PlayGamesPlatform.Instance.Authenticate((success) =>
         {
             if (success == SignInStatus.Success)
@@ -24,6 +56,7 @@ public class GooglePlayGameControl : MonoBehaviour
                 {
                     Debug.Log("Authorization code: " + code);
                     Token = code;
+                    Debug.Log("Token: " + Token);
                     // This token serves as an example to be used for SignInWithGooglePlayGames
                 });
             }
@@ -33,66 +66,47 @@ public class GooglePlayGameControl : MonoBehaviour
                 Debug.Log("Login Unsuccessful");
             }
         });
-    }
+#endif
 
-    public void SocialLogin()
-    {
-        Social.localUser.Authenticate(success => {
-            if (success)
-            {
-                Debug.Log("Authentication successful");
-                string userInfo = "Username: " + Social.localUser.userName +
-                                  "\nUser ID: " + Social.localUser.id +
-                                  "\nIsUnderage: " + Social.localUser.underage +
-                                  "\nToken ID: " + Social.localUser.id;
-                Debug.Log(userInfo);
-            }
-            else
-                Debug.Log("Authentication failed");
-        });
-
-    }
-
-    public void Login()
-    {
-        PlayGamesPlatform.Instance.Authenticate((success) =>
+#if UNITY_IOS
+        // Initialize the Apple Auth Manager
+        if (m_AppleAuthManager == null)
         {
-            if (success == SignInStatus.Success)
-            {
-                Debug.Log("Login with Google Play games successful.");
+            Initialize();
+        }
 
-                PlayGamesPlatform.Instance.RequestServerSideAccess(true, code =>
+        // Set the login arguments
+        var loginArgs = new AppleAuthLoginArgs(LoginOptions.IncludeEmail | LoginOptions.IncludeFullName);
+
+        // Perform the login
+        m_AppleAuthManager.LoginWithAppleId(loginArgs, credential =>
+            {
+                var appleIDCredential = credential as IAppleIDCredential;
+                if (appleIDCredential != null)
                 {
-                    Debug.Log("Authorization code: " + code);
-                    Token = code;
-                    // This token serves as an example to be used for SignInWithGooglePlayGames
-                });
-            }
-            else
+                    var idToken = Encoding.UTF8.GetString(appleIDCredential.IdentityToken,0,appleIDCredential.IdentityToken.Length);
+                    Debug.Log("Sign-in with Apple successfully done. IDToken: " + idToken);
+                    Token = idToken;
+                }
+                else
+                {
+                    Debug.Log("Sign-in with Apple error. Message: appleIDCredential is null");
+                    Debug.LogError("Retrieving Apple Id Token failed.");
+                }
+            },
+            error =>
             {
-                Debug.LogError("Failed to retrieve Google play games authorization code");
-                Debug.Log("Login Unsuccessful");
+                Debug.Log("Sign-in with Apple error. Message: " + error);
+                Debug.LogError("Retrieving Apple Id Token failed.");
             }
-        });
+        );
+#endif
+
     }
 
-    internal void ProcessAuthentication(SignInStatus status)
-    {
-        Debug.Log("status: " + status);
-        if (status == SignInStatus.Success)
-        {
-            // Continue with Play Games Services
-        }
-        else
-        {
-            // Disable your integration with Play Games Services or show a login button
-            // to ask users to sign-in. Clicking it should call
-            PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication);
-        }
-    }
 
     public void ShowToken()
     {
-        Debug.Log("PlayGamesPlatform.Instance.localUser.id: " + Token);
+        Debug.Log("Token: " + Token);
     }
 }
